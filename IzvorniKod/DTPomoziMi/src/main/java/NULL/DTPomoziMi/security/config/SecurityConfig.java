a998426c90f8c4ffe21aeacc4e4f13c9d0641bc4
Jan Roček <jan.rocek@fer.hr>
2020-11-06 22:40:04 +0100

    Added cors WebConfig

--- IzvorniKod/DTPomoziMi/src/main/java/NULL/DTPomoziMi/security/config/SecurityConfig.java
+++ IzvorniKod/DTPomoziMi/src/main/java/NULL/DTPomoziMi/security/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import NULL.DTPomoziMi.web.filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,9 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -91,26 +87,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(11);
-    }
-    
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    	CorsConfiguration conf = new CorsConfiguration();
-    	
-    	conf.setAllowCredentials(true);
-    	conf.addAllowedOrigin("*");
-    	conf.addAllowedHeader("*");
-    	conf.addAllowedMethod(HttpMethod.OPTIONS);
-    	conf.addAllowedMethod(HttpMethod.POST);
-    	conf.addAllowedMethod(HttpMethod.GET);
-    	conf.addAllowedMethod(HttpMethod.PUT);
-    	conf.addAllowedMethod(HttpMethod.DELETE);
-    	conf.addAllowedMethod(HttpMethod.PATCH);
-    	source.registerCorsConfiguration("/api/**", conf);
-    	
-    	return source;
-    }
-    
+    } 
     
 }
