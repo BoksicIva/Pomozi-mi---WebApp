0538dced316306e2c69990413b5b8de4f9bcce8d
IvaBoksic <ib51425@fer.hr>
2020-12-27 14:29:46 +0100

    Configure router

--- IzvorniKod/DTPomoziMi/src/main/webapp/src/App.js
+++ IzvorniKod/DTPomoziMi/src/main/webapp/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/login';
-import Registration from './components/registration';
-import Page from './components/requestLoader';
-import home from './components/home';
-import BasicTable from './components/userList';
+
+const Login = lazy(() => import("./components/login"));
+const Home = lazy(() => import("./components/home"));
+const Registration = lazy(() => import("./components/registration"));
+const RequestLoader = lazy(() => import("./components/requestLoader"));
+const UserList = lazy(() => import("./components/userList"));
+
 
 function App() {
   return (
+
     <>
-    <BrowserRouter>
-    <Switch>
-      <Route path='/page' component={Page}/>
-      <Route path='/list' component={BasicTable}/>
-      <Route path='/home' component={home}/>
-      <Route path='/login' component={Login}/>
-      <Route path='/register' exact component={Registration}/>
-      <Route path='/' component={Login}/>
-    </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path='/page' component={RequestLoader} />
+            <Route path='/list' component={UserList} />
+            <Route path='/home' component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' exact component={Registration} />
+            <Route path='/' component={Login} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
     </>
   );
 }
