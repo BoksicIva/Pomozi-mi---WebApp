91b7492c0bda0e1085f0c27135657901b6515376
IvaBoksic <ib51425@fer.hr>
2020-12-27 12:33:41 +0100

    Sidebar final look

--- IzvorniKod/DTPomoziMi/src/main/webapp/src/components/SidebarData.js
+++ IzvorniKod/DTPomoziMi/src/main/webapp/src/components/SidebarData.js
@@ -1,43 +1,38 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
+import * as BsIcons from "react-icons/bs";
 
 export const SidebarData = [
   {
-    title: 'Home',
-    path: '/',
-    icon: <AiIcons.AiFillHome />,
-    cName: 'nav-text'
+    title: 'Naslovnica',
+    path: '/home',
+    icon: <AiIcons.AiFillHome value={{ color: 'blue' }} />,
+    cName: 'nav-text',
   },
   {
-    title: 'Reports',
-    path: '/reports',
+    title: 'Zahtjevi',
+    path: '/request',
     icon: <IoIcons.IoIosPaper />,
     cName: 'nav-text'
   },
   {
-    title: 'Products',
-    path: '/products',
-    icon: <FaIcons.FaCartPlus />,
+    title: 'Profil',
+    path: '/profil',
+    icon: <BsIcons.BsPersonFill />,
     cName: 'nav-text'
   },
   {
-    title: 'Team',
-    path: '/team',
+    title: 'Korisnici',
+    path: '/users',
     icon: <IoIcons.IoMdPeople />,
     cName: 'nav-text'
   },
   {
-    title: 'Messages',
-    path: '/messages',
-    icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Support',
-    path: '/support',
-    icon: <IoIcons.IoMdHelpCircle />,
+    title: 'Odjava',
+    path: '/logout',
+    icon: <IoIcons.IoMdLogOut />,
     cName: 'nav-text'
   }
+
 ];
--- IzvorniKod/DTPomoziMi/src/main/webapp/src/components/sidebar.js
+++ IzvorniKod/DTPomoziMi/src/main/webapp/src/components/sidebar.js
@@ -1,47 +1,69 @@
 import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import * as ImIcons from "react-icons/im";
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
+import LogoutService from "../service/login-service";
 import './style/sidebar.css';
 import { IconContext } from 'react-icons';
 
+function Navbar(props) {
+  const [sidebar, setSidebar] = useState(false);
 
-function Navbar() {
-    const [sidebar, setSidebar] = useState(false);
-  
-    const showSidebar = () => setSidebar(!sidebar);
-  
-    return (
-      <>
-        <IconContext.Provider value={{ color: '#fff' }}>
-          <div className='navbar'>
-            <Link to='#' className='menu-bars'>
-              <FaIcons.FaBars onClick={showSidebar} />
-            </Link>
-          </div>
-          <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
-              <li className='navbar-toggle'>
-                <Link to='#' className='menu-bars'>
-                  <AiIcons.AiOutlineClose />
-                </Link>
-              </li>
-              {SidebarData.map((item, index) => {
-                return (
-                  <li key={index} className={item.cName}>
-                    <Link to={item.path}>
-                      {item.icon}
-                      <span>{item.title}</span>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </IconContext.Provider>
-      </>
-    );
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const handleLogOut = () => {
+    localStorage.removeItem("username");
+    
+    LogoutService.logout().then((response) => {
+        props.history.push('/login');
+    }).catch((error) => {
+        console.log(error);
+    });
   }
-  
-  export default Navbar;
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#000', size: "1.5em"}}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+          <h1 className="title"> Pomozi mi &nbsp;
+            <IconContext.Provider value={{ color: '#cc0000' }}>
+              <ImIcons.ImHeart></ImIcons.ImHeart>
+            </IconContext.Provider>
+          </h1>
+          
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineLeft/>
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon }
+                    if({item.title}=="Odjava"){
+                      <span onClick={handleLogOut}>{item.title}</span>
+                    }else{
+                      <span>{item.title}</span>
+                    }
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
+
