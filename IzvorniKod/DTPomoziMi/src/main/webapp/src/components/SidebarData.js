5176ff33e1bdafb6ffe118101d9b7edbcc4767f4
IvaBoksic <ib51425@fer.hr>
2021-01-01 17:21:42 +0100

    Dodan pregled zahtjeva u sidebar

--- IzvorniKod/DTPomoziMi/src/main/webapp/src/components/SidebarData.js
+++ IzvorniKod/DTPomoziMi/src/main/webapp/src/components/SidebarData.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 import * as BsIcons from "react-icons/bs";
+import * as BiIcon from "react-icons/bi";
 
 export const SidebarData = [
   {
@@ -11,15 +12,21 @@ export const SidebarData = [
     cName: 'nav_text',
   },
   {
-    title: 'Zahtjevi',
-    path: '/page',
-    icon: <IoIcons.IoIosPaper />,
-    cName: 'nav_text'
-  },
-  {
     title: 'Profil',
     path: '/profile',
     icon: <BsIcons.BsPersonFill />,
+    cName: 'nav_text'
+  },
+  {
+    title: 'Zadavanje zahtjeva',
+    path: '/page',
+    icon: <BiIcon.BiAddToQueue/>,
+    cName: 'nav_text'
+  },
+  {
+    title: 'Pregled zahtjeva',
+    path: '/requests',
+    icon: <IoIcons.IoIosPaper />,
     cName: 'nav_text'
   },
   {
