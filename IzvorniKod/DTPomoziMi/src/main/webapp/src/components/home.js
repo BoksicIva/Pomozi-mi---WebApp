31bb7b6ea34d9ad2d3740ab967caf7d48e807dc8
Robert <rd51779@fer.hr>
2021-01-03 16:09:28 +0100

    Ureden header, dodano dohvacanje tokena

--- IzvorniKod/DTPomoziMi/src/main/webapp/src/components/home.js
+++ IzvorniKod/DTPomoziMi/src/main/webapp/src/components/home.js
@@ -1,26 +1,21 @@
-import React from 'react'
-import homeStyle from './style/home.module.css'
+import React from "react";
+import homeStyle from "./style/home.module.css";
 
 import Sidebar from "./sidebar";
 import Slideshow from "./slider";
 
-
 export default function home(props) {
-    let name = localStorage.getItem("username");
-    
- 
- 
-
-    return (
-        <>
-        <Sidebar /> 
-        <img src="../images/holding-hands.png" alt=""/>
-        <div className = {homeStyle.container}>
+  return (
+    <>
+      <Sidebar />
+      <img src="../images/holding-hands.png" alt="" />
+      <div className={homeStyle.container}>
         <div className={homeStyle.pomozi}>Pomozi mi</div>
-        <div className={homeStyle.msg}>Malo djelo velikog<span className={homeStyle.znacaja}> značaja</span>.
-        </div> 
-        </div>  
-        <Slideshow/> 
-        </>
-    )
+        <div className={homeStyle.msg}>
+          Malo djelo velikog<span className={homeStyle.znacaja}> značaja</span>.
+        </div>
+      </div>
+      <Slideshow />
+    </>
+  );
 }
